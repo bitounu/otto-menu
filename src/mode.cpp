cb7d443aaebe53f9abfbca452201b16f755a65dc
Ryan Alexander <scloopy@gmail.com>
2015-01-27 00:33:25 +0000

    Initial commit of cmake build and deps

---
+++ src/mode.cpp
@@ -0,0 +1,19 @@
+#include "gfx.hpp"
+
+extern "C" {
+  int init();
+  int shutdown();
+  int update();
+}
+
+int init() {
+  return 0;
+}
+
+int shutdown() {
+  return 0;
+}
+
+int update() {
+  return 0;
+}
