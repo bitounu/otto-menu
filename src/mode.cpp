9ab721af0d8819ac6229cebf506535b3db30224d
Ryan Alexander <scloopy@gmail.com>
2015-01-29 03:57:11 +0000

    Begin mocking out the mode switcher

--- src/mode.cpp
+++ src/mode.cpp
@@ -1,6 +1,11 @@
 #include "gfx.hpp"
+#include "glm.hpp"
+#include "gtx/rotate_vector.hpp"
+#include "nanosvg.h"
+#include "choreograph/Choreograph.h"
 
 #include <iostream>
+#include <vector>
 
 extern "C" {
   int init();
@@ -9,10 +14,39 @@ extern "C" {
   int rotary_changed(int delta);
 }
 
-static float rotation = 0.0f;
+static const float screenWidth  = 96.0f;
+static const float screenHeight = 96.0f;
+
+static ch::Timeline timeline;
+static ch::Output<float> rotation;
+
+struct Tile {
+  glm::vec2 position;
+  float rotation;
+  NSVGimage image;
+};
+
+static float regularPolyRadius(float sideLen, uint32_t numSides) {
+  return sideLen / (2.0f * std::sin(M_PI / numSides));
+}
+
+static std::array<Tile, 6> tiles;
+static float tileDiameter = screenWidth * 0.95f;
+static float wheelEdgeLen = screenWidth * 1.1f;
+static float wheelRadius = regularPolyRadius(wheelEdgeLen, tiles.size());
 
 int init() {
-  std::cout << "HELLO!!!" << std::endl;
+  // Init tiles
+  {
+    float angle = 0.0f;
+    float angleIncr = (M_PI * 2.0f) / tiles.size();
+    for (auto &tile : tiles) {
+      tile.rotation = angle;
+      tile.position = glm::rotate(glm::vec2(0.0f, -wheelRadius), angle);
+      angle += angleIncr;
+    }
+  }
+
   return 0;
 }
 
@@ -23,37 +57,32 @@ int shutdown() {
 int update() {
   using namespace otto;
 
-  static const float screenWidth  = 96.0f;
-  static const float screenHeight = 96.0f;
   static const float defaultMatrix[] =
     { -0.0f, -1.0f, -0.0f, -1.0f,  0.0f,  0.0f, screenWidth, screenHeight, 1.0f };
 
   static const VGfloat bgColor[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+  timeline.step(1.0f / 30.0f);
 
   vgSetfv(VG_CLEAR_COLOR, 4, bgColor);
   vgClear(0, 0, 96, 96);
 
   vgLoadMatrix(defaultMatrix);
   vgTranslate(48, 48);
+  vgTranslate(0, wheelRadius);
   vgRotate(rotation);
 
-  {
-    auto path = vgCreatePath(VG_PATH_FORMAT_STANDARD, VG_PATH_DATATYPE_F, 1.0f, 0.0f, 0, 0, VG_PATH_CAPABILITY_ALL);
-
-    moveTo(path, 0, 0);
-    lineTo(path, 48, 0);
-
-    strokeWidth(5.0f);
-    strokeColor(1, 1, 0);
-
-    vgDrawPath(path, VG_STROKE_PATH);
-    vgDestroyPath(path);
+  fillColor(0, 1, 1);
+  for (const auto &tile : tiles) {
+    beginPath();
+    arc(tile.position.x, tile.position.y, tileDiameter, tileDiameter, 0, M_PI * 2.0f);
+    fill();
   }
 
   return 0;
 }
 
 int rotary_changed(int delta) {
-  rotation += delta * 5.0f;
+  timeline.apply(&rotation).then<ch::RampTo>(rotation() + delta * 10.0f, 0.1f);
   return 0;
 }
